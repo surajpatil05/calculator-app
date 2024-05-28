bcf7db8203c764da680154120b378ce5640a64fd
Suraj Patil <surajpatil05@gmail.com>
2024-05-28 11:53:09 +0530

    completed the app dev

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculator_app/calculator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Calculator(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
